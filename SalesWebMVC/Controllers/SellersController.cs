dc9b2e2ec21c478c0c2e0ce5aef8e6bc353571c0
vingadorv <vinifranca13@gmail.com>
2021-12-22 19:45:18 -0300

    Delete  Seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -40,5 +40,29 @@ namespace SalesWebMVC.Controllers
             _sellersService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Seller seller = _sellersService.FindById(id.Value);
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellersService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
